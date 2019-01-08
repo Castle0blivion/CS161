7b698471dae1d030d495fc691f5816cd38cd04dd
Castle0blivion <46452046+Castle0blivion@users.noreply.github.com>
2019-01-08 11:45:56 -0800

    Create ip1b_nicolec_victoriab_diagram.cpp

---
+++ ip1b_nicolec_victoriab_diagram.cpp
@@ -0,0 +1,31 @@
+// Filename: ip1b_victoriaB_nicolec_diagram.cpp
+// Description: program to draw a picture with symbols using escape sequences cout, \n \t
+// coder: Nicole Covey
+// navigator: Victoria Binkley
+// 1/8/2019
+
+#include <iostream>
+using namespace std;
+
+// main function
+int main()
+{
+    cout << "\t\t#\n"
+         << "\t\t\b#\n"
+         << "\t\t\b\b#\n"
+         << "\t\t\b\b\b####\n"
+         << "\t\t\b#\n"
+         << "\t\t\b\b#\n"
+         << "\t\t\b\b\b#\n"
+         << "\t\t\b\b\b\b#\n" // ligtning bolt
+         << "    @@@\t\t    @@@\n"
+         << "   @   @\t   @   @\n"
+         << "  @     @         @     @\n"
+         << " @       @       @       @\n"
+         << "@@       @=====@         @@\n"
+         << " @      @       @        @\n"
+         << "  @    @         @      @\n" // glasses
+         << "   @  @           @    @\n"
+         << "    @@\t\t    @@";
+}
+// the boy who lived
